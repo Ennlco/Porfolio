da6f17acaa657603b56fc16da8c19b90bd0fe196
Etienne Colmant <colmant.etienne@gmail.com>
2024-11-21 21:03:44 +0100

    first commit

---
+++ Javascript/Data_Realisation.js
@@ -0,0 +1,66 @@
+const real_list = [
+    {
+        id: "0",
+        titre: "Booki - projet Font",
+        img_Fond: "Ressource/Img_Realisation/Realisation_Booki.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus vulputate tellus, vitae faucibus odio elementum at. Sed at mi eget augue lacinia viverra. Duis id ligula vel ipsum semper tempor. Aliquam tincidunt mattis nunc, id semper arcu. Mauris a urna id purus varius auctor at eu nulla. Mauris id aliquam felis, eu posuere ex. Quisque vitae eleifend odio. Sed efficitur mi sit amet nunc posuere fringilla.",
+        code_Langage: [
+            "Ressource/Icones/Html.jpg",
+            "Ressource/Icones/Css.jpg"
+        ],
+        categorie: "pro",
+        gitHub: "https://github.com/Ennlco/Projet-HTML-et-CSS"
+    },
+    {
+        id: "1",
+        titre: "Sophie Bluel - projet Font",
+        img_Fond: "Ressource/Img_Realisation/Realisation_Sophie_Blueli.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus vulputate tellus, vitae faucibus odio elementum at. Sed at mi eget augue lacinia viverra. Duis id ligula vel ipsum semper tempor. Aliquam tincidunt mattis nunc, id semper arcu. Mauris a urna id purus varius auctor at eu nulla. Mauris id aliquam felis, eu posuere ex. Quisque vitae eleifend odio. Sed efficitur mi sit amet nunc posuere fringilla.",
+        code_Langage: [
+            "Ressource/Icones/Html.jpg",
+            "Ressource/Icones/Css.jpg",
+            "Ressource/Icones/Javascript.jpg"
+        ],
+        categorie: "pro",
+        gitHub: "https://github.com/Ennlco/Creez_une_page_web_dynamique_avec_JavaScript"
+    },
+    {
+        id: "2",
+        titre: "Kasa - projet React",
+        img_Fond: "Ressource/Img_Realisation/Realisation_Kasa.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus vulputate tellus, vitae faucibus odio elementum at. Sed at mi eget augue lacinia viverra. Duis id ligula vel ipsum semper tempor. Aliquam tincidunt mattis nunc, id semper arcu. Mauris a urna id purus varius auctor at eu nulla. Mauris id aliquam felis, eu posuere ex. Quisque vitae eleifend odio. Sed efficitur mi sit amet nunc posuere fringilla.",
+        code_Langage: [
+            "Ressource/Icones/React.jpg",
+            "Ressource/Icones/Sass.jpg",
+            "Ressource/Icones/Javascript.jpg"
+        ],
+        categorie: "pro",
+        gitHub: "https://github.com/Ennlco/Creez-une-application-web-de-location-immobili-re-avec-React"
+    },
+    {
+        id: "3",
+        titre: "Vieux Grimoire - projet Back",
+        img_Fond: "Ressource/Img_Realisation/Realisation_Mon_Vieux_Grimoire.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus vulputate tellus, vitae faucibus odio elementum at. Sed at mi eget augue lacinia viverra. Duis id ligula vel ipsum semper tempor. Aliquam tincidunt mattis nunc, id semper arcu. Mauris a urna id purus varius auctor at eu nulla. Mauris id aliquam felis, eu posuere ex. Quisque vitae eleifend odio. Sed efficitur mi sit amet nunc posuere fringilla.",
+        code_Langage: [
+            "Ressource/Icones/NodeJs.jpg",
+        ],
+        categorie: "pro",
+        gitHub: "https://github.com/Ennlco/Developpez-le-backend-d-un-site-de-notation-de-livres"
+    },
+    {
+        id: "4",
+        titre: "Star Wars - projet Front/Back",
+        img_Fond: "Ressource/Img_Realisation/Realisation_Star_Wars.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus vulputate tellus, vitae faucibus odio elementum at. Sed at mi eget augue lacinia viverra. Duis id ligula vel ipsum semper tempor. Aliquam tincidunt mattis nunc, id semper arcu. Mauris a urna id purus varius auctor at eu nulla. Mauris id aliquam felis, eu posuere ex. Quisque vitae eleifend odio. Sed efficitur mi sit amet nunc posuere fringilla.",
+        code_Langage: [
+            "Ressource/Icones/Html.jpg",
+            "Ressource/Icones/Css.jpg",
+            "Ressource/Icones/Javascript.jpg",
+            "Ressource/Icones/Php.jpg",
+            "Ressource/Icones/MySql.jpg"
+        ],
+        categorie: "perso",
+        gitHub: "https://github.com/Ennlco/JDR-Star-Wars"
+    }
+]
---
+++ Javascript/Index.js
@@ -0,0 +1,269 @@
+const Base = document.querySelector(".content_Real")
+const fond = document.querySelector(".modale_Info")
+const elementReal = real_list
+
+
+function generation(elementReal){
+    Base.innerHTML = ""
+
+    elementReal.forEach((e) =>{
+        const divBase = document.createElement("div")
+        divBase.className = "div_Real"
+        divBase.style.backgroundImage = "URL(" + e.img_Fond + ")"
+        Base.appendChild(divBase)
+
+        const divInfo = document.createElement("div")
+        divInfo.className = "info"
+        divBase.appendChild(divInfo)
+
+        const h5 = document.createElement("h5")
+        h5.innerText = e.titre
+        divInfo.appendChild(h5)
+
+        const divIcone = document.createElement("div")
+        divIcone.classList = "content_icone"
+        divInfo.appendChild(divIcone)
+
+        let arrayIcone = e.code_Langage
+
+        arrayIcone.forEach((a) => {
+            const imgIcone = document.createElement("img")
+            imgIcone.src = a
+            imgIcone.alt = "Icone Langage"
+            imgIcone.classList = "icone"
+            divIcone.appendChild(imgIcone)
+        })
+
+        const divClick = document.createElement("div")
+        divClick.className = "element_click"
+        divClick.id = e.id
+        divBase.appendChild(divClick)
+    })
+}
+
+function modaleRealisation(){
+    let selectReal = document.querySelectorAll(".element_click")
+    const modale = document.querySelector(".content_modale_info")
+
+    for(i = 0; i < selectReal.length; i++){
+        selectReal[i].addEventListener("click", (event) =>{
+            let clickId = event.target.id
+            fond.style.display = "flex"
+            modale.innerHTML = ""
+
+            const cross = document.createElement("i")
+            cross.className = "fa-solid fa-xmark"
+            modale.appendChild(cross)
+
+            const h3 = document.createElement("h3")
+            h3.innerText = real_list[clickId].titre
+            modale.appendChild(h3)
+
+            const divSep = document.createElement("div")
+            divSep.className = "line_Sep_modale"
+            modale.appendChild(divSep)
+
+            const imgGallery = document.createElement("img")
+            imgGallery.src = real_list[clickId].img_Fond
+            imgGallery.alt = `image ${real_list[clickId].titre}`
+            imgGallery.className = "gallery_Modale"
+            modale.appendChild(imgGallery)
+
+            const description = document.createElement("p")
+            description.innerText = real_list[clickId].description
+            modale.appendChild(description)
+
+            const contentDivIcone = document.createElement("div")
+            contentDivIcone.className = "content_modale_icone"
+            modale.appendChild(contentDivIcone)
+
+            let arrayIcone = real_list[clickId].code_Langage
+
+            arrayIcone.forEach((e) => {
+                const imgIcone = document.createElement("img")
+                imgIcone.src = e
+                imgIcone.alt = "Icone Langage"
+                imgIcone.classList = "icone_modale"
+                contentDivIcone.appendChild(imgIcone)
+            })
+
+            const divLink = document.createElement("div")
+            divLink.className = "content_link"
+            modale.appendChild(divLink)
+
+            const iconeGit = document.createElement("div")
+            iconeGit.className = "git_icone"
+            divLink.appendChild(iconeGit)
+
+            const linkGit = document.createElement("a")
+            linkGit.innerText = "Lient GitHub"
+            linkGit.href = real_list[clickId].gitHub
+            linkGit.className = "link_git"
+            divLink.appendChild(linkGit)
+        })
+    }
+
+    fond.addEventListener("click", () =>{
+        fond.style.display = "none"
+    })
+}
+
+function filtres(){
+
+    const selectFiltre = document.querySelectorAll(".filtre")
+
+    for(i = 0; i < selectFiltre.length; i++){
+        selectFiltre[i].addEventListener("click", (event) =>{
+
+            if(event.target.id == "tous"){
+                const pro = document.getElementById("pro")
+                const perso = document.getElementById("perso")
+                
+                event.target.classList.add ("active")
+                pro.classList.remove("active")
+                perso.classList.remove("active")
+
+                generation(elementReal)
+                modaleRealisation()
+            }
+            if(event.target.id == "pro"){
+                const realPro = real_list.filter(function(list){
+                    return list.categorie === event.target.id
+                })
+                const tous = document.getElementById("tous")
+                const perso = document.getElementById("perso")
+                
+                event.target.classList.add ("active")
+                tous.classList.remove("active")
+                perso.classList.remove("active")
+
+                generation(realPro)
+                modaleRealisation()
+            }
+            if(event.target.id == "perso"){
+                const realPerso = real_list.filter(function(list){
+                    return list.categorie === event.target.id
+                })
+                const pro = document.getElementById("pro")
+                const tous = document.getElementById("tous")
+                
+                event.target.classList.add ("active")
+                pro.classList.remove("active")
+                tous.classList.remove("active")
+                
+                generation(realPerso)
+                modaleRealisation()
+            }
+        })
+    }
+}
+
+function closeAllInfo(){
+    const eInfo1 = document.querySelector(".f1")
+    const eInfo2 = document.querySelector(".f2")
+    const eInfo3 = document.querySelector(".f3")
+    const eInfo4 = document.querySelector(".e1")
+    const eInfo5 = document.querySelector(".e2")
+    const eInfo6 = document.querySelector(".e3")
+
+    eInfo1.style.display = "none"
+    eInfo2.style.display = "none"
+    eInfo3.style.display = "none"
+    eInfo4.style.display = "none"
+    eInfo5.style.display = "none"
+    eInfo6.style.display = "none"
+}
+
+function openInfo(){
+    const cevronAll = document.querySelectorAll(".fa-caret-up")
+    
+    for(i = 0; i < cevronAll.length; i++){
+        cevronAll[i].addEventListener("click", (event) =>{
+
+            if(event.target.classList.contains("close") === true){
+                event.target.classList.remove("close")
+                event.target.classList.add("open")
+            } else {
+                event.target.classList.remove("open")
+                event.target.classList.add("close")
+                closeAllInfo()
+            }
+
+            if(event.target.id == "form_1" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".f1")
+                eInfo.style.display = "flex"
+            }
+
+            if(event.target.id == "form_2" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".f2")
+                eInfo.style.display = "flex"
+            }
+
+            if(event.target.id == "form_3" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".f3")
+                eInfo.style.display = "flex"
+            }
+
+            if(event.target.id == "exp_1" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".e1")
+                eInfo.style.display = "flex"
+            }
+
+            if(event.target.id == "exp_2" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".e2")
+                eInfo.style.display = "flex"
+            }
+
+            if(event.target.id == "exp_3" && event.target.classList.contains("open") === true){
+                closeAllInfo()
+                const eInfo = document.querySelector(".e3")
+                eInfo.style.display = "flex"
+            }  
+        })
+    }
+}
+
+function navMobile(){
+    const clickMobile = document.querySelector(".click_nav")
+    const navOpen = document.querySelector(".content_nav_mobile")
+
+    const line1 = document.querySelector(".line_nav_1")
+    const line2 = document.querySelector(".line_nav_2")
+    const line3 = document.querySelector(".line_nav_3")
+
+    clickMobile.addEventListener("click", () =>{
+
+        if(navOpen.id == "close_nav"){
+            
+            navOpen.style.display = "flex"
+            navOpen.id = "open_nav"
+            navOpen.classList.add("close_select")
+            navOpen.classList.remove("open_select")
+            line1.style.transform = "rotate(45deg)"
+            line2.style.opacity = "0"
+            line3.style.transform = "rotate(-45deg)"
+
+        } else {
+            
+            navOpen.style.display = "none"
+            navOpen.id = "close_nav"
+            navOpen.classList.remove("close_select")
+            navOpen.classList.add("open_select")
+
+            line1.style.transform = "rotate(0deg)"
+            line2.style.opacity = "1"
+            line3.style.transform = "rotate(0deg)"
+        }
+    })
+}
+
+generation(elementReal)
+modaleRealisation()
+filtres()
+openInfo()
+navMobile()
